2d712e47bbf7c4a46bdc97585f847ada23c96c6f
Julio Bacoli <julio-microlins@hotmail.com>
2024-08-20 17:17:45 -0300

    adicionando implementações

--- ChamadaTelefonicaTwilio/Program.cs
+++ ChamadaTelefonicaTwilio/Program.cs
@@ -2,32 +2,32 @@
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 
-static void Main(string[] args)
-{
-    string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
-    string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
 
-    string callNumerTo = Environment.GetEnvironmentVariable("CALL_NUMBER_TO");
-    string callNumerFromTwilio = Environment.GetEnvironmentVariable("CALL_NUMBER_FROM_TWILIO");
+string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
 
-    TwilioClient.Init(accountSid, authToken);
+string callNumerTo = Environment.GetEnvironmentVariable("CALL_NUMBER_TO");
+string callNumerFromTwilio = Environment.GetEnvironmentVariable("CALL_NUMBER_FROM_TWILIO");
 
-    StringBuilder sb = new StringBuilder();
+TwilioClient.Init(accountSid, authToken);
 
-    sb.AppendLine("<Response>");
-    sb.AppendLine("<Say language='pt-BR'>");
-    sb.AppendLine("Olá, tudo bem?");
-    sb.AppendLine("Estou ligando para você para te lembrar de algo importante.");
-    sb.AppendLine("Não se esqueça de fazer o backup dos seus arquivos.");
-    sb.AppendLine("Tenha um bom dia!");
-    sb.AppendLine("<Response>");
+StringBuilder sb = new StringBuilder();
 
-    var call = CallResource.Create(
-        twiml: sb.ToString(),
-        to: new Twilio.Types.PhoneNumber(callNumerTo),
-        from: new Twilio.Types.PhoneNumber(callNumerFromTwilio)
-    );
+sb.AppendLine("<Response>");
+sb.AppendLine("<Say language='pt-BR'>");
+sb.AppendLine("Olá, tudo bem?");
+sb.AppendLine("Estou ligando para você para te lembrar de algo importante.");
+sb.AppendLine("Não se esqueça de fazer o backup dos seus arquivos.");
+sb.AppendLine("Tenha um bom dia!");
+sb.AppendLine("<Response>");
 
-    Console.WriteLine(call.Sid);
-    Console.WriteLine(call.Uri.ToString());
-}
+var call = CallResource.Create(
+    twiml: sb.ToString(),
+    to: new Twilio.Types.PhoneNumber(callNumerTo),
+    from: new Twilio.Types.PhoneNumber(callNumerFromTwilio)
+);
+
+Console.WriteLine(call.Sid);
+Console.WriteLine(call.Uri.ToString());
+Console.WriteLine(call.Duration);
+
