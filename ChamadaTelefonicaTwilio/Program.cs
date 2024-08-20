176f770643064115c67123d726f829854553b8d4
Julio Bacoli <julio-microlins@hotmail.com>
2024-08-20 16:20:32 -0300

    Adicionar arquivos de projeto.

---
+++ ChamadaTelefonicaTwilio/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+static void Main(string[] args)
+{
+    string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+    string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+    string callNumerTo = Environment.GetEnvironmentVariable("CALL_NUMBER_TO");
+    string callNumerFromTwilio = Environment.GetEnvironmentVariable("CALL_NUMBER_FROM_TWILIO");
+
+    TwilioClient.Init(accountSid, authToken);
+
+    StringBuilder sb = new StringBuilder();
+
+    sb.AppendLine("<Response>");
+    sb.AppendLine("<Say language='pt-BR'>");
+    sb.AppendLine("Olá, tudo bem?");
+    sb.AppendLine("Estou ligando para você para te lembrar de algo importante.");
+    sb.AppendLine("Não se esqueça de fazer o backup dos seus arquivos.");
+    sb.AppendLine("Tenha um bom dia!");
+    sb.AppendLine("<Response>");
+
+    var call = CallResource.Create(
+        twiml: sb.ToString(),
+        to: new Twilio.Types.PhoneNumber(callNumerTo),
+        from: new Twilio.Types.PhoneNumber(callNumerFromTwilio)
+    );
+
+    Console.WriteLine(call.Sid);
+    Console.WriteLine(call.Uri.ToString());
+}
